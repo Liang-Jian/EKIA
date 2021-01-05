e0018a3d112afede58b1c5d8308f97c02ac680bb
Liang-Jian <devilsct@gmail.com>
2021-01-05 20:28:12 +0800

    fix bet code

--- Python/spierfb/jleague_fix.py
+++ Python/spierfb/jleague_fix.py
@@ -1,14 +1,13 @@
 
-import datetime
-import time
-import mysql.connector
-import configparser
-import re
+import datetime,time,mysql.connector,configparser,re,requests,yaml
 import lxml.html
 import requests
 from bs4 import BeautifulSoup
 from Python.spierfb.logmodule import Logi,Loge,Logd,Logc
 from abc import ABCMeta,abstractclassmethod
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 from selenium import webdriver
@@ -30,56 +29,55 @@ Team_A = {
     "爱媛FC":"愛媛","山口雷诺":"山口","甲府风林":"甲府","大宫松鼠":"大宮",
     "岐阜FC":"岐阜","大分三神":"大分","町田泽维":"町田","新泻天鹅":"新潟",
     "京都":"京都","赞岐釜玉":"讃岐","熊本深红":"熊本","群马温泉":"群馬",
-    "东京绿茵":"東京Ｖ","横滨FC":"横浜FC","枥木SC":"栃木"
+    "东京绿茵":"東京Ｖ","横滨FC":"横浜FC","枥木SC":"栃木","北九州":"北九州",
+    "琉球FC":"琉球"
 }
 def getkey(_dict,_value):
     return list(_dict.keys())[list(_dict.values()).index(_value)]
 
-class shujuku(metaclass=ABCMeta):
+cf = configparser.ConfigParser()
+cf.read("../spierfb/peizhi.ini")
 
-    @abstractclassmethod
-    def search_db(self,*args):
-        pass
-    @abstractclassmethod
-    def update_db(self,*args):
-        pass
-    @classmethod
-    def close_db(self):
-        pass
 
-class mysqlOP(shujuku):
+file = open("../spierfb/run.yml", "r", encoding="utf8")
+conff = yaml.load(file.read(), Loader=yaml.Loader)
+conf = conff['control']
+
+
+
+class MsqService(object):
 
     def __init__(self):
-        self.conn = mysql.connector.connect(host='118.25.78.198', user='root', password='root', database='jleague',
-                                            use_unicode=True)
-
-    def search_db(self,sql):
+        self.conn = mysql.connector.connect(host='118.25.78.198', user='root', password='root',
+                                            database='jleague',use_unicode=True)
+    def search(self, sql):
         self.cursor = self.conn.cursor()
         self.cursor.execute(sql)
         select_data = self.cursor.fetchall()
-        Logi("查询结果{}".format(select_data))
+        Logi("sql result:={}".format(select_data))
         return select_data
 
-    def update_db(self,sql):
+    def update(self, sql):
         self.cursor.execute(sql)
         self.conn.commit()
 
-    def close_db(self):
+    def close(self):
         if self.conn != None:
             self.conn.close()
+
         if self.cursor !=None:
             self.cursor.close()
 
-cf = configparser.ConfigParser()
-cf.read("../spierfb/peizhi.ini")
-class GetJleagueData(mysqlOP):
+
+
+class GetJleagueData(MsqService):
     #需要修改start_url ,_get1url中start ,end轮数
     def __init__(self):
         super(GetJleagueData,self).__init__()
 
-        self.start_roud= 10  #开始轮数
-        self.end_round = 11  #结束轮数
-        self.level     = "A"
+        self.start_roud= conf['startNo']  #开始轮数
+        self.end_round = conf['endNo']    #结束轮数
+        self.level     = conf['level']
         self.round     = ""
         self.st        = ""
         self.status    = False
@@ -90,8 +88,7 @@ class GetJleagueData(mysqlOP):
         self.kc        = ""
         self.start_url = "https://www.jleague.jp/match/section/{}/{}/"
         self.headers   = {'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-        # self.proxies   = {'http': 'socks5://127.0.0.1:1080', 'https': 'socks5://127.0.0.1:1080'}
-        self.proxies   = {'http': 'socks5://127.0.0.1:10808', 'https': 'socks5://127.0.0.1:10808'}
+        self.proxies   = {'http': 'socks5://127.0.0.1:1080', 'https': 'socks5://127.0.0.1:1080'}
 
     def _get1url(self,):
         #第一级URL start开始轮数
@@ -108,9 +105,10 @@ class GetJleagueData(mysqlOP):
         secondURL= []
         _str = "https://www.jleague.jp"
         for i in self._get1url():
-            html = requests.get(i, verify=True, headers=self.headers) #proxies=self.proxies,
+            html = requests.get(i, verify=False, headers=self.headers) #proxies=self.proxies,
+            # html = requests.get(i, verify=False, headers=self.headers, proxies=self.proxies) #
             k = re.findall("href=\"(.*?)\">試合詳細",html.text)
-            print(k)
+            # print(k)
             if k != []:
                 for s in k:
                     secondURL.append(_str+s)
@@ -125,15 +123,13 @@ class GetJleagueData(mysqlOP):
                 return level_pool.get(key)
 
     def get_sql(self,url):
-        # self.html = requests.get(url, proxies=self.proxies, verify=True, headers=self.headers)
-        self.html = requests.get(url, verify=True, headers=self.headers)
+        self.html = requests.get(url, verify=False, headers=self.headers)
         selector =lxml.html.fromstring(self.html.text)
         soup = BeautifulSoup(self.html.text, 'lxml')
 
         new_url = url.replace("live/","ajax_live?_={}T{}") #天气信息ajax动态加载的.拼ajax的url
         new_url.format(datetime.date.today(),time.strftime('%H:%M',time.localtime(time.time())))
-        # weather_web = requests.get(new_url, proxies=self.proxies, verify=True, headers=self.headers).content.decode()
-        weather_web = requests.get(new_url, verify=True, headers=self.headers).content.decode()
+        weather_web = requests.get(new_url, verify=False, headers=self.headers).content.decode()
         weather_soup = BeautifulSoup(weather_web, 'lxml')
         _zhu = weather_soup.table.strings
         s = []
@@ -152,12 +148,13 @@ class GetJleagueData(mysqlOP):
         self.st      = "".join(selector.xpath(cf.get("css","sj_css"))).replace(":","")
         self.round   = re.findall("\d+","".join(selector.xpath(cf.get("css","level_css"))),re.S)[1]
         self.date    = re.findall("2020/(.*?)/live",soup.link['href'])[0]
+        print(re.findall("(.*?)\/", s[-3]))
         self.weather = "".join(re.findall("(.*?)\/", s[-3])[0].split())[0:1]
 
         _insertsql = "INSERT INTO `j8` VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (
             self.date, self.st, self.level, self.round, self.weather, self.zhu, "", self.ke, self.bc,
             self.zj, self.kj, self.zc, self.kc, "9.99", "9.99", "9.99")
-        Logi("creatsql {}".format(_insertsql))
+        Logi("sql:={}".format(_insertsql))
         return  _insertsql
 
     def _writeDB(self, sqlyuju):
@@ -169,20 +166,20 @@ class GetJleagueData(mysqlOP):
         if data is []:
             Loge("匹配数据失败")
         _date = data[0]
-        if self.search_db("select * from j8 where date ='%s'" % _date) != []:
+        if self.search("select * from j8 where date ='%s'" % _date) != []:
             Loge("'%s'数据库中已存在" % _date)
         else:
-            self.update_db(sql_lan)
+            self.update(sql_lan)
             Logi("数据插入成功")
 
-    def _get_all_sql(self):#返回所有的sql语句
+    def _get_all_sql(self): #返回所有的sql语句
         all_sql = []
         for i in self._get2url():
             try:
                 all_sql.append(self.get_sql(i))
             except:
                 raise("数据返回失败")
-        Logi("全部sql返回成功" + self._get_all_sql.__name__+ "")
+        Logi("all sql:=" % all_sql)
         return all_sql
 
     def run(self):
@@ -190,77 +187,15 @@ class GetJleagueData(mysqlOP):
             try:
                 self._writeDB(k)
             except:
-                Loge("{}插入失败".format(k))
-'''
-class UIGetWeatherData(mysqlOP):#通过selenium获取天气信息 ,现在不用了.可以通过get ajaxurl来去数据
-    weather_css = "#loadArea > table > tbody > tr:nth-child(3) > td:nth-child(2)"
-    def __init__(self):
-
-        super(UIGetWeatherData, self).__init__()
-        profile_dir = "/home/root/.mozilla/firefox/291hkm8s.root"
-        profile = webdriver.FirefoxProfile(profile_dir)
-        self.driver = webdriver.Firefox(profile)
-        self.timeout= 30
-        self._level = "A"
-    def _geturl(self):
-        #查找天气为空的比赛
-        __url = []
-        if   self._level == "A":
-            url_model = "https://www.jleague.jp/match/j1/2018/{}/live/#live"   #j1修改如果是
-        elif self._level == "B":
-            url_model = "https://www.jleague.jp/match/j2/2018/{}/live/#live"
-        _sql = "select weather,date from j8 where  level='%s'" % self._level
-        data = self.search_db(_sql)
-        _del = []
-        for i in data:
-            if i[0] == "":
-                _del.append(i[1])
-        for s in _del:
-            _s = str(s)
-            if len(_s) < 6:
-                _s = "0"+str(_s)
-                _j_url = url_model.format(_s)
-            __url.append(_j_url)
-            logger_info("JURL拼接成功{}".format(_j_url))
-        return __url
-
-    def _openB_getW(self,url):
-        #开浏览器获取数据,返回单个sql语句
-        self.driver.maximize_window()
-        self.driver.get(url)
-        _weather_text = WebDriverWait(self.driver, self.timeout, 0.5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, UIGetWeatherData.weather_css))).text
-        # print(_weather_text)
-        logger_info("天气获取成功{}".format(_weather_text))
-        try:
-            weather_text = "".join(re.findall("(.*?)\/", _weather_text)[0].split())[0:1]
-            update_sql = "update j8 set weather='%s' where date='%s'" % (weather_text,re.findall("2018/(.*?)/live",url)[0])
-            logger_info("天气更新语句生成成功{}".format(update_sql))
-            return update_sql
-        except:
-            logger_warning("天气更新语句生成失败{}".format(update_sql))
-
-    def _get_insert_sql(self):
-        #返回所有sql语句列表
-        insert_sql = []
-        for i in self._geturl():
-            insert_sql.append(self._openB_getW(i))
-        return insert_sql
-    def run(self):
-        #插入数据
-        for i in self._get_insert_sql():
-            try:
-                self.update_db(i)
-            except:
-                logger_warning("{}插入失败".format(i))
-        self.driver.quit()
-'''
-class Get500Data(mysqlOP):
+                continue
+                Loge("{} insert fail".format(k))
+class Get500Data(MsqService):
     #需要修改self._level 的级别
     def __init__(self):
         super(Get500Data, self).__init__()
 
         self.headers = {'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-        self._level  = "A"
+        self._level  = conf['level']
         self._zhu    = ""
         self._date   = ""
         self._asia   = ""
@@ -268,60 +203,60 @@ class Get500Data(mysqlOP):
         self._draw   = ""
         self._lose   = ""
         self._timeout= 30
-    def _getmatchtime(self):
-        #数据库中查找asia为空的数据的所有数据,返回date % 0224列表
+    def get_matchdate(self):
+        #数据库中查找asia为空的数据的所有数据,返回date % 0224
         match_date = []
-        list_ = self.search_db("select distinct(left(date,4)),asia from j8 where level ='%s'" % self._level)
+        list_ = self.search("select distinct(left(date,4)),asia from j8 where level ='%s'" % self._level)
         # print (list_)
         for i in list_:
             if i[1] == "":
                 match_date.append(i[0])
-        Logi("查找asia字段成功{}".format(match_date))
+        Logi("asia null date:={}".format(match_date))
         return match_date
 
-    def _get5url(self):
-
-        pin_all_url = []
-        url_tmp = "http://live.500.com/?e=2020-"
-        for i in self._getmatchtime():
+    def get5url(self):
+        # return all 500 url ,
+        url_500 = []
+        url_tmp = "http://live.500.com/?e={}-".format(conf['year'])
+        for i in self.get_matchdate():
             _url_ = url_tmp+i[0:2]+"-"+i[2:4]
-            pin_all_url.append(_url_)
-            Logi("500url生成成功{}".format(_url_))
-        return pin_all_url
+            url_500.append(_url_)
+            Logi("500 url={}".format(_url_))
+        return url_500
 
     def _getupdatesql(self):
         update_sql = []
-        for i in self._get5url():
+        for i in self.get5url():
             update_sql +=self._getdata(i)
-        # print(update_sql)
+        print(update_sql)
         Logi("全部sql生成成功")
         return  update_sql
 
-    @staticmethod
-    def getzhuandke(self):
-        #查到主客队信息,格式如下{'Ｃ大阪': '横浜FM'}
-        dist ={}
-        _zhu_info =[]
-        _sql = "select zhu,ke from j8 where asia = '' and left(date,4)={} and level='{}';"
-        for i in self._getmatchtime():
-            _zhu_info += self.search_db(_sql.format(i,self._level))
-        for i in _zhu_info:
-            dist.update({i[0]: i[1]})
-        Logi("主客队生成成功"+self.getzhuandke.__name__)
-        print(dist)
-        return dist
+    def get_match_team(self,date):
+        match_team_dict = dict()
+        match_team_list = list()
+        update_sql = "select zhu,ke from j8 where asia = '' and left(date,4)={} and level='{}';"
+        match_team_list += self.search(update_sql.format(date,self._level))
+        match_team_dict = dict(match_team_list)
+
+        return match_team_dict
+
 
     def _getdata(self,url):
         _urll = []
         file_txt = requests.get(url=url,headers=self.headers).content.decode("gbk").encode("utf-8").decode("utf-8")
-        date4 = url[-5::].replace("-","")
+        match_date = url[-5::].replace("-","")
 
-        team = self.getzhuandke(self)
+        team = self.get_match_team(match_date)
         for key,val in team.items():
             self._zhu =getkey(Team_A,key)
             self._ke  =getkey(Team_A,val)
             # logger_info("主队 {},客队 {}".format(self._zhu,self._ke))
-            _datadict = eval(re.findall("var liveOddsList = (.*?);", file_txt, re.S)[0]) #所有赔率列表
+            # print(re.findall("var liveOddsList = (.*?);", file_txt, re.S)[0])
+            try:
+                _datadict = eval(re.findall("var liveOddsList = (.*?);", file_txt, re.S)[0])
+            except Exception as e:
+                print(e)
             k = re.findall("<tr(.*?)"+self._zhu+","+self._ke,file_txt)
             # print(k)
             if k !=[]:
@@ -335,13 +270,13 @@ class Get500Data(mysqlOP):
                 self._asia = re.findall("\">(.*?)$",level1)[0]
                 updatesql = (
                             "update j8 set asia='%s',win='%s',draw='%s',lose='%s' where zhu='%s' and left(date,4)='%s'" % (
-                    self._asia, self._win, self._draw, self._lose, Team_A[self._zhu], date4))
+                    self._asia, self._win, self._draw, self._lose, Team_A[self._zhu], match_date))
                 _urll.append(updatesql)
                 Logi("sql增加成功{}".format(updatesql))
             else:
                 pass
         return(_urll)
-    def _writeDb(self,sqlyuju):
+    def writeSql(self, sqlyuju):
 
         sql_lan = sqlyuju
         if sql_lan is None:
@@ -350,16 +285,16 @@ class Get500Data(mysqlOP):
         _left_date = re.findall("\)=\'(.*?)\'",sql_lan,re.S)[0]
         Logi("插入前查询{} {}".format(_zhu,_left_date))
         if _zhu is not []:
-            if self.search_db("select asia from j8 where zhu ='%s' and left(date,4)='%s' " % (_zhu,_left_date))[0][0] != "":
+            if self.search("select asia from j8 where zhu ='%s' and left(date,4)='%s' " % (_zhu, _left_date))[0][0] != "":
                 Logi("数据不为空 {}".format(sql_lan))
             else:
-                self.update_db(sql_lan)
+                self.update(sql_lan)
                 Logi("数据插入成功 {}".format(sql_lan))
         else:
             Loge("zhu列匹配数据失败")
     def run(self):
         for i in self._getupdatesql():
-            self._writeDb(i)
+            self.writeSql(i)
 def main():
     GetJleagueData().run()
     Get500Data().run()
