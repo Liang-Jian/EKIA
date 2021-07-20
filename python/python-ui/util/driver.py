3b46b6527661d97030f929f5172fabc6cba4082a
Liang-Jian <devilsct@gmail.com>
2021-07-20 16:37:43 +0800

    删除公司ui代码

--- python/python-ui/util/driver.py
+++
@@ -1,9 +0,0 @@
-
-from interface.interfaceDriver import DriVer
-from selenium import webdriver
-
-
-class StartDriver(DriVer):
-    def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
